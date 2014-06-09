84bd2de6d5d63d7096b04334c711ada458b7397d
wouldyougo <wouldyougo@gmail.com>
2014-06-09 14:10:46 +0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 #SampleApp::Application.routes.draw do
-  get 'users/new'
+  #get 'users/new'
+  resources :users
 
 #  get 'static_pages/home'
 #  get 'static_pages/help'
