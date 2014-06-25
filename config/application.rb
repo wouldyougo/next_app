0e9c34009adee310c97905add5eb53b03e519ef2
wouldyougo <wouldyougo@gmail.com>
2014-06-25 21:12:56 +0400

    Начало локализации

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module SampleApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)    
-    I18n.enforce_available_locales = true    
+    I18n.enforce_available_locales = true
+    config.i18n.default_locale = :ru
   end
 end
