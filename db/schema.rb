e75232913ccced5b36fb91bb34c7224fbd987445
wouldyougo <wouldyougo@gmail.com>
2014-06-30 20:37:41 +0400

    scaffold Order db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140627154841) do
+ActiveRecord::Schema.define(version: 20140630163141) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
@@ -21,6 +21,32 @@ ActiveRecord::Schema.define(version: 20140627154841) do
   end
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+
+  create_table "orders", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "reader_id"
+    t.string   "order_status"
+    t.datetime "contract_date"
+    t.string   "abonent"
+    t.string   "address"
+    t.string   "phone"
+    t.string   "phone_mobile"
+    t.datetime "connection_date"
+    t.string   "inet_tariff_plan"
+    t.string   "inet_device_contract"
+    t.integer  "inet_installment_plan"
+    t.string   "iptv_tariff_plan"
+    t.string   "iptv_device_contract"
+    t.integer  "iptv_installment_plan"
+    t.string   "passport_series"
+    t.string   "passport_number"
+    t.datetime "passport_registration_date"
+    t.string   "passport_issued_by"
+    t.datetime "date_of_birth"
+    t.string   "place_of_birth"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "relationships", force: true do |t|
     t.integer  "follower_id"
