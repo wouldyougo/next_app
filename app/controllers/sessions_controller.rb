05010c0a8599f3a77fc6ae658439bc23fcbb9333
wouldyougo <wouldyougo@gmail.com>
2014-07-02 13:37:15 +0400

    Improve the Order

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+      redirect_to root_url
+      #redirect_to user
       #redirect_back_or user
     else
       flash.now[:error] = 'Invalid email/password combination'
